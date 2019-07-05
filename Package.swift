bc440724ada8be766c058ce12dd85c0dbd9a4460
Sergey Di <sergey_di@icloud.com>
2019-07-05 10:44:53 +0300

    Migrate to Swift PM4

--- Package.swift
+++ Package.swift
@@ -1,5 +1,26 @@
+// swift-tools-version:4.0
 import PackageDescription
 
 let package = Package(
-    name: "Swinject"
+    name: "Swinject",
+    products: [
+
+    ],
+    dependencies: [
+      .package(url: "https://github.com/Quick/Quick", from: "1.1.0"),
+      .package(url: "https://github.com/Quick/Nimble", from: "7.0.0")
+    ],
+    targets: [
+        .target(
+            name: "Swinject",
+            dependencies: [],
+            path: "Sources"),
+        .testTarget(
+            name: "SwinjectTests",
+            dependencies: [
+                "Quick",
+                "Nimble",
+                "Swinject"
+            ])
+    ]
 )
